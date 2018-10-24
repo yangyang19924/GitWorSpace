96a4be851b1bc4a374410a0accbced158a6f5920
yangyang <2211325707@qq.com>
2018-10-24 15:19:45 +0800

    111
    111.1
    
    111.2
    
    111.2.1

---
+++ Test/src/main/java/TestRebase.java
@@ -0,0 +1,14 @@
+/**
+ * Created by yangyang on 2018/10/24.
+ */
+public class TestRebase {
+
+    public static void main(String[] args) {
+        System.out.println("test commit 1");
+        System.out.println("test commit 1");
+        System.out.println("test commit 1.1");
+        System.out.println("test commit 1.1");
+        System.out.println("test commit 1.2.1");
+        System.out.println("test commit 1.2.1");
+    }
+}
